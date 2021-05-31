054cfdd0b71c70a11285bbedc4b3b3ceb131db1b
isaacbatst <isaacbatst@gmail.com>
2021-05-31 15:45:16 -0300

    add numeration

--- data.js
+++ data.js
@@ -100,9 +100,9 @@ const data = {
   }
 }
 
-// Quanto custa o sanduíche trybe?
-// Montar e validar sanduíche recebendo ingredientes e retornar o preço 
-// Calcular menu com promoção baseado no dia
-// Procurar pizzas mais baratas que X
-// Calcular preço final e quanto fica pra cada amigo
-// Retornar lanches e preço dos combos
+// 1.Quanto custa o sanduíche trybe?
+// 2.Montar e validar sanduíche recebendo ingredientes e retornar o preço 
+// 3.Calcular menu com promoção baseado no dia
+// 4.Procurar pizzas mais baratas que X
+// 5.Calcular preço final e quanto fica pra cada amigo
+// 6.Retornar lanches e preço dos combos
