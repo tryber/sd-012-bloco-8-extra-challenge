39959a3628dc8a0f3774c20d32424714f37d4d6e
Henrique JensenD <henriquejensen@hotmail.com>
2021-06-02 14:52:52 -0300

    remove solution

--- src/challenges.js
+++ src/challenges.js
@@ -1,150 +1,22 @@
 // 1.Quanto custa o sanduíche trybe?
-// 2.Montar e validar sanduíche recebendo ingredientes e retornar o preço 
+// 2.Montar e validar sanduíche recebendo ingredientes e retornar o preço
 // 3.Calcular menu com promoção baseado no dia
 // 4.Procurar pizzas mais baratas que X
 // 5.Calcular preço final e quanto fica pra cada amigo
 // 6.Retornar lanches e preço dos combos
-const data = require('../data');
+const data = require("../data");
 
-function getIngredientsPrice(ingredients) {
-  return ingredients.reduce((acc, id) => {
-    const ingredient = data.menu.sandwichItems.find(ingredient => ingredient.id === id);
+function getSandwichPrice(sandwichName) {}
 
-    if (ingredient) {
-      return ingredient.price + acc;
-    }
+function getDayMenu(day) {}
 
-    return acc; //lançar erro?
-  }, 0)
-}
+function getSharedBill(ordersIDs, qtd) {}
 
-function getSandwichPrice(sandwichName) {
-  const sandwich = data.premade.sandwichs.find(sandwich => sandwich.name === sandwichName);
+function makeSandwich(items) {}
 
-  if (sandwich) {
-    const price = getIngredientsPrice(sandwich.ingredients);
+function getCheapestPizza(price) {}
 
-    return price;
-  }
-
-  throw new Error('Sanduíche não encontrado');
-}
-
-function getDiscountedMenu() {
-  const discount = 0.1;
-
-  const pizzasDiscounted = data.menu.pizzas.map(pizza => ({
-    ...pizza,
-    price: pizza.price - (pizza.price * discount),
-  }))
-
-  return {
-    ...data.menu,
-    pizzas: pizzasDiscounted
-  }
-}
-
-function getDayMenu(day) {
-  if (data.saleDays.includes(day)) {
-    return getDiscountedMenu();
-  }
-
-  return data.menu;
-}
-
-function findById(id) {
-  const foundPizza = data.menu.pizzas.find(pizza => pizza.id === id)
-
-  if (foundPizza) {
-    return foundPizza;
-  }
-
-  const foundDrink = data.menu.drinks.find(drink => drink.id === id);
-
-  if (foundDrink) {
-    return foundDrink;
-  }
-
-  throw new Error('Produto não encontrado');
-}
-
-function getSharedBill(ordersIDs, qtd) {
-  const total = ordersIDs.reduce((acc, order) => {
-    const product = findById(order.id);
-    const totalProductPrice = product.price * order.qtd;
-
-    return acc + totalProductPrice;
-  }, 0);
-
-  return total / qtd;
-}
-
-function reduceArrayToObject(array, prop) {
-  return array
-    .reduce((acc, item) => ({ ...acc, [item.name]: item }), {});
-}
-
-
-function makeSandwich(items) {
-  const sandwichItems = reduceArrayToObject(data.menu.sandwichItems);
-
-  if (items.some(item => !sandwichItems[item])) {
-    throw new Error(`Pedido inválido`);
-  }
-
-  return items
-    .map(item => ({ ...sandwichItems[item] }))
-    .reduce((acc, item) => acc + item.price, 0);
-}
-
-function getCheapestPizza(price) {
-  return data.menu.pizzas.filter(pizza => pizza.price < price);
-}
-
-function getSandwichById(id) {
-  return data.premade.sandwichs.find(sand => sand.id === id);
-}
-
-function getDrinkById(id) {
-  return data.menu.drinks.find(drink => drink.id === id);
-}
-
-function getPizzaById(id) {
-  return data.menu.pizzas.find(pizza => pizza.id === id);
-}
-
-function reduceItemsId(itemsId) {
-  return itemsId.reduce((items, id) => {
-    const sandwich = getSandwichById(id);
-    const drink = getDrinkById(id);
-    const pizza = getPizzaById(id);
-    let price = 0;
-
-    if (sandwich) {
-      items.sandwich = sandwich.name;
-      price = price + getSandwichPrice(sandwich.name);
-    }
-    if (drink) {
-      items.drink = drink.name;
-      price = price + drink.price;
-    }
-    if (pizza) {
-      items.pizza = pizza.name;
-      price = price + pizza.price;
-    }
-    items.price = items.price + price;
-    return items;
-  }, { price: 0 })
-}
-
-function getComboSandwichsAndPrices() {
-  const { combos } = data.premade;
-
-  return combos.map((combo) => ({
-    name: combo.name,
-    ...reduceItemsId(combo.items)
-  }))
-}
+function getComboSandwichsAndPrices() {}
 
 module.exports = {
   getSandwichPrice,
@@ -152,6 +24,5 @@ module.exports = {
   getSharedBill,
   makeSandwich,
   getCheapestPizza,
-  getComboSandwichsAndPrices
-}
-
+  getComboSandwichsAndPrices,
+};
